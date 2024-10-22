803d2b874a7a823af83de297adcc07436b96a049
mig1023 <mig1023@yandex.ru>
2024-10-22 14:21:16 +0300

    init

---
+++ SeekerMAUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SeekerMAUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ SeekerMAUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SeekerMAUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
