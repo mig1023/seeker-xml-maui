86f6fbe0350a706e7160145329888dd098bba950
mig1023 <mig1023@yandex.ru>
2024-10-22 14:26:39 +0300

    Gamebook codes (antiquary)

---
+++ SeekerMAUI/Gamebook/DeathOfAntiquary/Actions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace SeekerMAUI.Gamebook.DeathOfAntiquary
+{
+    class Actions : Prototypes.Actions, Abstract.IActions
+    {
+        public override bool Availability(string option) =>
+            AvailabilityTrigger(option);
+    }
+}
