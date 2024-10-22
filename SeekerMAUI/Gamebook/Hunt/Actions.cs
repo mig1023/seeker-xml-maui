ce49f84391b7dcf3823d7cbc64a335d4f00e636e
mig1023 <mig1023@yandex.ru>
2024-10-22 14:28:10 +0300

    Gamebook code (hunt)

---
+++ SeekerMAUI/Gamebook/Hunt/Actions.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SeekerMAUI.Gamebook.Hunt
+{
+    class Actions : Prototypes.Actions, Abstract.IActions
+    {
+        public override List<string> Status()
+        {
+            string zombies = new string('x', Character.Protagonist.Bitten).Replace("x", "🧟");
+            string bitten = String.IsNullOrEmpty(zombies) ? "ни одного" : zombies;
+            return new List<string>{ $"Укушенные: {bitten}" };
+        }
+
+        public override bool Availability(string option)
+        {
+            if (String.IsNullOrEmpty(option))
+            {
+                return true;
+            }
+            else if (option.Contains("|"))
+            {
+                return option.Split('|').Where(x => Game.Option.IsTriggered(x.Trim())).Count() > 0;
+            }
+            else if (!option.Contains(","))
+            {
+                if (option.Contains("ЦИКЛ >"))
+                {
+                    return Character.Protagonist.Cycle > Game.Services.LevelParse(option);
+                }
+                else if (option.Contains("УКУШЕННЫЕ >="))
+                {
+                    return Character.Protagonist.Bitten >= Game.Services.LevelParse(option);
+                }
+                else if (option.Contains("УКУШЕННЫЕ <"))
+                {
+                    return Character.Protagonist.Bitten < Game.Services.LevelParse(option);
+                }
+                else
+                {
+                    return AvailabilityTrigger(option);
+                }
+            }
+            else
+            {
+                foreach (string oneOption in option.Split(','))
+                {
+                    if (oneOption.Contains("!"))
+                    {
+                        if (Game.Option.IsTriggered(oneOption.Replace("!", String.Empty).Trim()))
+                            return false;
+                    }
+                    else if (!Game.Option.IsTriggered(oneOption.Trim()))
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+        }
+    }
+}
---
+++ SeekerMAUI/Gamebook/Hunt/Modification.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace SeekerMAUI.Gamebook.Hunt
+{
+    class Modification : Prototypes.Modification, Abstract.IModification
+    {
+        private void RandomTrigger(string trigger)
+        {
+            int random = Game.Dice.Roll(size: 10);
+            Game.Option.Trigger($"{random}{trigger}");
+        }
+
+        private void ProbabilityTrigger(int probability, string trigger)
+        {
+            if (Game.Dice.Roll(size: probability) == 1)
+                Game.Option.Trigger(trigger);
+        }
+
+        private void Clean(string trigger)
+        {
+            for (int i = 1; i <= 10; i++)
+                Game.Option.Trigger($"{i}{trigger}", remove: true);
+        }
+
+        public override void Do()
+        {
+            if (Name == "RandomAction")
+            {
+                RandomTrigger("действие");
+            }
+            else if (Name == "CleanAction")
+            {
+                Clean("действие");
+            }
+            else if (Name == "RandomResponse")
+            {
+                RandomTrigger("ответ");
+            }
+            else if (Name == "CleanResponse")
+            {
+                Clean("ответ");
+            }
+            else if (Name == "RandomActionAndResponse")
+            {
+                RandomTrigger("действие");
+                RandomTrigger("ответ");
+            }
+            else if (Name == "RandomBit")
+            {
+                ProbabilityTrigger(probability: 2, "Укусил");
+            }
+            else if (Name == "RandomMiss")
+            {
+                ProbabilityTrigger(probability: 4, "Мимо");
+            }
+            else
+            {
+                base.Do(Character.Protagonist);
+            }
+        }
+    }
+}
