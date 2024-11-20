a0156294d4927faa67913d9c9f4fb7ab63f1f714
mig1023 <mig1023@yandex.ru>
2024-11-20 16:37:48 +0300

    Availability multi option (chain) #2

--- SeekerMAUI/Gamebook/KoshcheisChain/Actions.cs
+++ SeekerMAUI/Gamebook/KoshcheisChain/Actions.cs
@@ -69,6 +69,10 @@ namespace SeekerMAUI.Gamebook.KoshcheisChain
                 {
                     return level <= Character.Protagonist.Staff;
                 }
+                else if (option.Contains("ЖЕЗЛ ="))
+                {
+                    return level == Character.Protagonist.Staff;
+                }
                 else if (option.Contains(","))
                 {
                     var options = option.Split(",");
