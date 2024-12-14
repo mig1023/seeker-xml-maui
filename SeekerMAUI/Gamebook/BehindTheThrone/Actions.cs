7ad2efa59b82afaeb46eb8363583fae3c82c6e8e
mig1023 <mig1023@yandex.ru>
2024-12-14 19:53:53 +0300

    Character properties (diversant)

--- SeekerMAUI/Gamebook/BehindTheThrone/Actions.cs
+++ SeekerMAUI/Gamebook/BehindTheThrone/Actions.cs
@@ -2,5 +2,17 @@
 
 namespace SeekerMAUI.Gamebook.BehindTheThrone
 {
-    class Actions : Prototypes.Actions, Abstract.IActions { }
+    class Actions : Prototypes.Actions, Abstract.IActions
+    {
+        public override List<string> Status() => new List<string>
+        {
+            $"Проворство: {Character.Protagonist.Agility}",
+            $"Меткость: {Character.Protagonist.Marksmanship}",
+            $"Фехтование: {Character.Protagonist.Swashbuckling}",
+            $"Живучесть: {Character.Protagonist.Vitality}",
+        };
+
+        public override bool GameOver(out int toEndParagraph, out string toEndText) =>
+            GameOverBy(Character.Protagonist.Vitality, out toEndParagraph, out toEndText);
+    }
 }
--- SeekerMAUI/Gamebook/BehindTheThrone/Character.cs
+++ SeekerMAUI/Gamebook/BehindTheThrone/Character.cs
@@ -2,5 +2,58 @@
 
 namespace SeekerMAUI.Gamebook.BehindTheThrone
 {
-    class Character : Prototypes.Character, Abstract.ICharacter { }
+    class Character : Prototypes.Character, Abstract.ICharacter
+    {
+        public static Character Protagonist { get; set; }
+        public override void Set(object character) =>
+            Protagonist = (Character)character;
+
+        public int Agility { get; set; }
+
+        public int Marksmanship { get; set; }
+
+        public int Swashbuckling { get; set; }
+
+        private int _vitality;
+        public int Vitality
+        {
+            get => _vitality;
+            set => _vitality = Game.Param.Setter(value, _vitality, this);
+        }
+
+        public override void Init()
+        {
+            base.Init();
+
+            Agility = Game.Dice.Roll() + 3;
+            Marksmanship = Game.Dice.Roll() + 3;
+            Swashbuckling = Game.Dice.Roll() + 3;
+            Vitality = Game.Dice.Roll() + 10;
+        }
+
+        public Character Clone() => new Character()
+        {
+            IsProtagonist = this.IsProtagonist,
+            Name = this.Name,
+            Agility = this.Agility,
+            Marksmanship = this.Marksmanship,
+            Swashbuckling = this.Swashbuckling,
+            Vitality = this.Vitality,
+        };
+
+        public override string Save() => String.Join("|",
+            Agility, Marksmanship, Swashbuckling, Vitality);
+
+        public override void Load(string saveLine)
+        {
+            string[] save = saveLine.Split('|');
+
+            Agility = int.Parse(save[0]);
+            Marksmanship = int.Parse(save[1]);
+            Swashbuckling = int.Parse(save[2]);
+            Vitality = int.Parse(save[3]);
+
+            IsProtagonist = true;
+        }
+    }
 }
