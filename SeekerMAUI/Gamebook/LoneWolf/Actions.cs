6984d46b740050672906431a2a41d22dad7e186d
mig1023 <mig1023@yandex.ru>
2025-01-23 14:45:15 +0300

    Paragraph + Actions (lonewolf)

--- SeekerMAUI/Gamebook/LoneWolf/Actions.cs
+++ SeekerMAUI/Gamebook/LoneWolf/Actions.cs
@@ -2,5 +2,42 @@
 
 namespace SeekerMAUI.Gamebook.LoneWolf
 {
-    class Actions : Prototypes.Actions, Abstract.IActions { }
+    class Actions : Prototypes.Actions, Abstract.IActions
+    {
+        public bool Disciplines { get; set; }
+
+        public List<Character> Enemies { get; set; }
+
+        public override List<string> Status() => new List<string>
+        {
+            $"Боевой навык: {Character.Protagonist.Skill}",
+            $"Выносливость: {Character.Protagonist.Strength}/{Character.Protagonist.MaxStrength}",
+            $"Монеты: {Character.Protagonist.Gold}",
+        };
+
+        public override bool GameOver(out int toEndParagraph, out string toEndText) =>
+            GameOverBy(Character.Protagonist.Strength, out toEndParagraph, out toEndText);
+
+        public override bool IsButtonEnabled(bool secondButton = false)
+        {
+            if (Disciplines && Game.Option.IsTriggered(this.Button))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public List<string> Get()
+        {
+            if (Disciplines)
+            {
+                Game.Option.Trigger(this.Button);
+            }
+
+            return new List<string> { "RELOAD" };
+        }
+    }
 }
--- SeekerMAUI/Gamebook/LoneWolf/Paragraphs.cs
+++ SeekerMAUI/Gamebook/LoneWolf/Paragraphs.cs
@@ -1,6 +1,54 @@
-﻿using System;
+﻿using SeekerMAUI.Game;
+using System;
+using System.Xml;
 
 namespace SeekerMAUI.Gamebook.LoneWolf
 {
-    class Paragraphs : Prototypes.Paragraphs, Abstract.IParagraphs { }
+    class Paragraphs : Prototypes.Paragraphs, Abstract.IParagraphs
+    {
+        public override Paragraph Get(int id, XmlNode xmlParagraph) =>
+           base.Get(xmlParagraph, ParagraphTemplate(xmlParagraph));
+
+        public override Abstract.IActions ActionParse(XmlNode xmlAction)
+        {
+            Actions action = (Actions)ActionTemplate(xmlAction, new Actions());
+
+            foreach (string param in GetProperties(action))
+                SetProperty(action, param, xmlAction);
+
+            if (xmlAction["Enemy"] != null)
+            {
+                action.Enemies = new List<Character> { EnemyParse(xmlAction["Enemy"]) };
+            }
+            else if (xmlAction["Enemies"] != null)
+            {
+                action.Enemies = new List<Character>();
+
+                foreach (XmlNode xmlEnemy in xmlAction.SelectNodes("Enemies/Enemy"))
+                    action.Enemies.Add(EnemyParse(xmlEnemy));
+            }
+
+            if (action.Type == "Option")
+                action.Option = OptionParse(xmlAction);
+
+            action.Benefit = Xml.ModificationParse(xmlAction["Benefit"], new Modification());
+
+            return action;
+        }
+
+        public override Abstract.IModification ModificationParse(XmlNode xmlModification) =>
+            Xml.ModificationParse(xmlModification, new Modification());
+
+        private Character EnemyParse(XmlNode xmlEnemy)
+        {
+            Character enemy = new Character();
+
+            foreach (string param in GetProperties(enemy))
+                SetPropertyByAttr(enemy, param, xmlEnemy, maxPrefix: true);
+
+            enemy.Strength = enemy.MaxStrength;
+
+            return enemy;
+        }
+    }
 }
