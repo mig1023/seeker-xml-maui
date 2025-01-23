f0dbf723ccb8ba8e0f0e53813fe21d7ba6c0143a
mig1023 <mig1023@yandex.ru>
2025-01-23 14:39:26 +0300

    Additional Status fix (fifa)

--- SeekerMAUI/Gamebook/FIFA1966/Actions.cs
+++ SeekerMAUI/Gamebook/FIFA1966/Actions.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Linq;
 
 namespace SeekerMAUI.Gamebook.FIFA1966
@@ -17,6 +17,59 @@ namespace SeekerMAUI.Gamebook.FIFA1966
             else
             {
                 return new List<string>();
+            }
+        }
+
+        public override List<string> AdditionalStatus()
+        {
+            
+            var match = Character.Protagonist.Vars["расходники/номер игры"];
+
+            if (match <= 0)
+            {
+                return new List<string> { "Подготовка к Чемпионату мира" };
+            }
+            else if (match < 5)
+            {
+                var enemy = Constants.Matches[match];
+                return new List<string> { $"Подготовка к Чемпионату мира: {enemy}" };
+            }
+            else if (match < 8)
+            {
+                var statuses = new List<string>();
+
+                var group = Character.Protagonist.Vars
+                    .ToDictionary()
+                    .Where(x => x.Key.StartsWith("групповой этап/"))
+                    .OrderByDescending(x => x.Value);
+
+                int place = 0;
+
+                foreach (var team in group)
+                {
+                    var name = team.Key.Replace("групповой этап/", "");
+                    var value = Character.Protagonist.Vars[$"групповой этап/{name}"];
+
+                    place += 1;
+
+                    statuses.Add($"{place} место: {name} ({value} очков)");
+                }
+
+                return statuses;
+            }
+            else if (match < 10)
+            {
+                var enemy = Constants.Matches[match];
+                var round = match == 8 ? "Четвертьфинал" : "Полуфинал";
+                return new List<string> { $"{round}: {enemy}" };
+            }
+            else if ((match == 11) || (match == 81))
+            {
+                return new List<string> { $"Игра за бронзу: Португалия" };
+            }
+            else
+            {
+                return new List<string> { $"Финал: Англия" };
             }
         }
 
--- SeekerMAUI/Gamebook/FIFA1966/Constants.cs
+++ SeekerMAUI/Gamebook/FIFA1966/Constants.cs
@@ -5,5 +5,6 @@ namespace SeekerMAUI.Gamebook.FIFA1966
     class Constants : Prototypes.Constants, Abstract.IConstants
     {
         public static Dictionary<string, int> Teams { get; set; }
+        public static Dictionary<int, string> Matches { get; set; }
     }
 }
