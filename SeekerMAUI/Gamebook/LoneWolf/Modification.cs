48d98ffab4e8ca0f2077cf3820b80135874f1a02
mig1023 <mig1023@yandex.ru>
2025-01-27 13:25:08 +0300

    AvoidingCombat modification (lonewolf)

--- SeekerMAUI/Gamebook/LoneWolf/Modification.cs
+++ SeekerMAUI/Gamebook/LoneWolf/Modification.cs
@@ -2,5 +2,19 @@
 
 namespace SeekerMAUI.Gamebook.LoneWolf
 {
-    class Modification : Prototypes.Modification, Abstract.IModification { }
+    class Modification : Prototypes.Modification, Abstract.IModification
+    {
+        public override void Do()
+        {
+            if (Name == "AvoidingCombat")
+            {
+                var maxWound = Character.Protagonist.Strength - 1;
+                Character.Protagonist.Strength -= Game.Dice.Roll(size: maxWound);
+            }
+            else
+            {
+                base.Do(Character.Protagonist);
+            }
+        }
+    }
 }
