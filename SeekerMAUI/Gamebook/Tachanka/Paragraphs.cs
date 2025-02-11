4835e345e355e82c7fe7989489f980da0fd49e1d
mig1023 <mig1023@yandex.ru>
2025-02-11 13:55:26 +0300

    Goto by Random (tachanka)

--- SeekerMAUI/Gamebook/Tachanka/Paragraphs.cs
+++ SeekerMAUI/Gamebook/Tachanka/Paragraphs.cs
@@ -1,6 +1,44 @@
-﻿using System;
+﻿using SeekerMAUI.Game;
+using System;
+using System.Xml;
 
 namespace SeekerMAUI.Gamebook.Tachanka
 {
-    class Paragraphs : Prototypes.Paragraphs, Abstract.IParagraphs { }
+    class Paragraphs : Prototypes.Paragraphs, Abstract.IParagraphs
+    {
+        public override Paragraph Get(int id, XmlNode xmlParagraph)
+        {
+            Paragraph paragraph = ParagraphTemplate(xmlParagraph);
+
+            foreach (XmlNode xmlOption in xmlParagraph.SelectNodes("Options/*"))
+            {
+                Option option = OptionsTemplateWithoutGoto(xmlOption);
+
+                if (int.TryParse(xmlOption.Attributes["Goto"].Value, out int _))
+                {
+                    option.Goto = Xml.IntParse(xmlOption.Attributes["Goto"]);
+                }
+                else
+                {
+                    List<string> link = xmlOption.Attributes["Goto"].Value.Split(',').ToList<string>();
+                    option.Goto = int.Parse(link[random.Next(link.Count())]);
+                }
+
+                XmlNode optionMod = xmlOption.SelectSingleNode("*");
+
+                if ((optionMod != null) && (optionMod["Value"] != null))
+                    option.Do.Add(Xml.ModificationParse(optionMod, new Modification()));
+
+                paragraph.Options.Add(option);
+            }
+
+            foreach (XmlNode xmlAction in xmlParagraph.SelectNodes("Actions/*"))
+                paragraph.Actions.Add(ActionParse(xmlAction));
+
+            foreach (XmlNode xmlModification in xmlParagraph.SelectNodes("Modifications/*"))
+                paragraph.Modification.Add(ModificationParse(xmlModification));
+
+            return paragraph;
+        }
+    }
 }
