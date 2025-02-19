c5dae04d7e2c87cda4338cc9946488a8780ac0de
mig1023 <mig1023@yandex.ru>
2025-02-19 14:04:11 +0300

    Crew change fix (tachanka)

--- SeekerMAUI/Gamebook/Tachanka/Modification.cs
+++ SeekerMAUI/Gamebook/Tachanka/Modification.cs
@@ -9,7 +9,7 @@ namespace SeekerMAUI.Gamebook.Tachanka
             if (Name == "Crew")
             {
                 if (Character.Protagonist.Team.Count >= 3)
-                    Character.Protagonist.Team.RemoveAt(3);
+                    Character.Protagonist.Team.RemoveAt(2);
 
                 var crew = ValueString.Split(',');
                 Character.Protagonist.Team.Add(new Crew(crew[0], crew[1]));
