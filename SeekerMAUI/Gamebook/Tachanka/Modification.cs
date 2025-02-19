55bace9d0081e5499d7d135e6c960cc1f5b46377
mig1023 <mig1023@yandex.ru>
2025-02-19 14:08:56 +0300

    Crew Add with Replace (tachanka)

--- SeekerMAUI/Gamebook/Tachanka/Modification.cs
+++ SeekerMAUI/Gamebook/Tachanka/Modification.cs
@@ -4,12 +4,19 @@ namespace SeekerMAUI.Gamebook.Tachanka
 {
     class Modification : Prototypes.Modification, Abstract.IModification
     {
+        public bool AddWithReplace { get; set; }
+
         public override void Do()
         {
             if (Name == "Crew")
             {
                 if (Character.Protagonist.Team.Count >= 3)
+                {
+                    if (AddWithReplace)
+                        Character.Protagonist.Team[1] = Character.Protagonist.Team[2];
+
                     Character.Protagonist.Team.RemoveAt(2);
+                }
 
                 var crew = ValueString.Split(',');
                 Character.Protagonist.Team.Add(new Crew(crew[0], crew[1]));
