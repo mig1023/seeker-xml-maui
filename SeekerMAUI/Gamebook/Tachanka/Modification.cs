6373283e6cf832316e1f4640fbb4f6d221862649
mig1023 <mig1023@yandex.ru>
2025-02-21 15:40:09 +0300

    Lost mods (tachanka)

--- SeekerMAUI/Gamebook/Tachanka/Modification.cs
+++ SeekerMAUI/Gamebook/Tachanka/Modification.cs
@@ -21,6 +21,29 @@ namespace SeekerMAUI.Gamebook.Tachanka
                 var crew = ValueString.Split(',');
                 Character.Protagonist.Team.Add(new Crew(crew[0], crew[1]));
             }
+            else if (Name == "Lost")
+            {
+                if (ValueString == "All")
+                {
+                    Character.Protagonist.Team = new List<Crew>();
+                    return;
+                }
+
+                foreach (var name in ValueString.Split(','))
+                {
+                    var not = name.StartsWith("!");
+
+                    for (var i = 0; i < Character.Protagonist.Team.Count; i++)
+                    {
+                        var crew = Character.Protagonist.Team[i];
+                        if (!name.Contains(crew.Name) && not)
+                            Character.Protagonist.Team.RemoveAt(i);
+
+                        if (name.Contains(crew.Name) && !not)
+                            Character.Protagonist.Team.Remove(crew);
+                    }
+                }
+            }
             else
             {
                 base.Do(Character.Protagonist);
