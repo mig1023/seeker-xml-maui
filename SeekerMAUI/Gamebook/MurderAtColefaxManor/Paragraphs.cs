5bcef2a8e5fdba41e975d49870e031025262a04d
mig1023 <mig1023@yandex.ru>
2025-02-26 12:36:16 +0300

    Availability + Parsing (murder)

--- SeekerMAUI/Gamebook/MurderAtColefaxManor/Paragraphs.cs
+++ SeekerMAUI/Gamebook/MurderAtColefaxManor/Paragraphs.cs
@@ -1,6 +1,15 @@
-﻿using System;
+﻿using SeekerMAUI.Game;
+using System;
+using System.Xml;
 
 namespace SeekerMAUI.Gamebook.MurderAtColefaxManor
 {
-    class Paragraphs : Prototypes.Paragraphs, Abstract.IParagraphs { }
+    class Paragraphs : Prototypes.Paragraphs, Abstract.IParagraphs
+    {
+        public override Paragraph Get(int id, XmlNode xmlParagraph) =>
+            base.Get(xmlParagraph);
+
+        public override Option OptionParse(XmlNode xmlOption) =>
+            OptionParseWithDo(xmlOption, new Modification());
+    }
 }
