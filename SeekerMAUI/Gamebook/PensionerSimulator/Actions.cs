e5165c29908e29dae27968c3321f5bd6b7966dea
mig1023 <mig1023@yandex.ru>
2025-03-08 14:45:16 +0300

    AvailabilityNode (pensioner)

--- SeekerMAUI/Gamebook/PensionerSimulator/Actions.cs
+++ SeekerMAUI/Gamebook/PensionerSimulator/Actions.cs
@@ -4,25 +4,7 @@ namespace SeekerMAUI.Gamebook.PensionerSimulator
 {
     class Actions : Prototypes.Actions, Abstract.IActions
     {
-        public override bool Availability(string option)
-        {
-            if (String.IsNullOrEmpty(option))
-                return true;
-
-            foreach (string oneOption in option.Split(','))
-            {
-                bool negativeTrigger = Game.Option.IsTriggered(oneOption.Replace("!", String.Empty).Trim());
-
-                if (oneOption.Contains("!") && negativeTrigger)
-                    return false;
-
-                bool positiveTrigger = Game.Option.IsTriggered(oneOption.Trim());
-
-                if (!oneOption.Contains("!") && !positiveTrigger)
-                    return false;
-            }
-
-            return true;
-        }
+        public override bool AvailabilityNode(string option) =>
+            AvailabilityTrigger(option);
     }
 }
