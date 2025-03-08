9c381ae23e97906f775c82ae15cea24016c52184
mig1023 <mig1023@yandex.ru>
2025-03-08 14:42:31 +0300

    AvailabilityNode (alcatraz)

--- SeekerMAUI/Gamebook/Alcatraz/Actions.cs
+++ SeekerMAUI/Gamebook/Alcatraz/Actions.cs
@@ -4,29 +4,7 @@ namespace SeekerMAUI.Gamebook.Alcatraz
 {
     class Actions : Prototypes.Actions, Abstract.IActions
     {
-        public override bool Availability(string option)
-        {
-            if (String.IsNullOrEmpty(option))
-            {
-                return true;
-            }
-            else
-            {
-                foreach (string oneOption in option.Split(','))
-                {
-                    if (oneOption.Contains("!"))
-                    {
-                        if (Game.Option.IsTriggered(oneOption.Replace("!", String.Empty).Trim()))
-                            return false;
-                    }
-                    else if (!Game.Option.IsTriggered(oneOption.Trim()))
-                    {
-                        return false;
-                    }
-                }
-
-                return true;
-            }
-        }
+        public override bool Availability(string option) =>
+            AvailabilityTrigger(option);
     }
 }
