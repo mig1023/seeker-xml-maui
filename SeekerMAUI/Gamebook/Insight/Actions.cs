65cb2373a2effdf3abf876850f2f2e2c9a71e432
mig1023 <mig1023@yandex.ru>
2025-03-08 14:44:23 +0300

    AvailabilityNode (insight)

--- SeekerMAUI/Gamebook/Insight/Actions.cs
+++ SeekerMAUI/Gamebook/Insight/Actions.cs
@@ -70,36 +70,18 @@ namespace SeekerMAUI.Gamebook.Insight
             return !(disabledByBonusesRemove || disabledByBonusesAdd);
         }
 
-        public override bool Availability(string option)
+        public override bool AvailabilityNode(string option)
         {
-            if (String.IsNullOrEmpty(option))
+            if (Game.Services.AvailabilityByСomparison(option))
             {
-                return true;
+                var fail = Game.Services.AvailabilityByProperty(Character.Protagonist,
+                    option, Constants.Availabilities, onlyFailTrueReturn: true);
+
+                return !fail;
             }
             else
             {
-                foreach (string oneOption in option.Split(','))
-                {
-                    if (Game.Services.AvailabilityByСomparison(oneOption))
-                    {
-                        var fail = Game.Services.AvailabilityByProperty(Character.Protagonist,
-                            oneOption, Constants.Availabilities, onlyFailTrueReturn: true);
-
-                        if (fail)
-                            return false;
-                    }
-                    else if (oneOption.Contains("!"))
-                    {
-                        if (Game.Option.IsTriggered(oneOption.Replace("!", String.Empty).Trim()))
-                            return false;
-                    }
-                    else if (!Game.Option.IsTriggered(oneOption.Trim()))
-                    {
-                        return false;
-                    }
-                }
-
-                return true;
+                return AvailabilityTrigger(option);
             }
         }
 
