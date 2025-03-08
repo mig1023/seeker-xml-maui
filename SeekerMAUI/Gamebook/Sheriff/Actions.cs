f14babec54217c7e058aa2e70fdcf93508cc3a97
mig1023 <mig1023@yandex.ru>
2025-03-08 14:46:10 +0300

    AvailabilityNode (sheriff)

--- SeekerMAUI/Gamebook/Sheriff/Actions.cs
+++ SeekerMAUI/Gamebook/Sheriff/Actions.cs
@@ -6,31 +6,17 @@ namespace SeekerMAUI.Gamebook.Sheriff
     {
         public override bool Availability(string option)
         {
-            if (String.IsNullOrEmpty(option))
-                return true;
-
-            foreach (string oneOption in option.Split(','))
+            if (Game.Services.AvailabilityByСomparison(option))
             {
-                if (Game.Services.AvailabilityByСomparison(oneOption))
-                {
-                    var fail = Game.Services.AvailabilityByProperty(Character.Protagonist,
-                        oneOption, Constants.Availabilities, onlyFailTrueReturn: true);
+                var fail = Game.Services.AvailabilityByProperty(Character.Protagonist,
+                    option, Constants.Availabilities, onlyFailTrueReturn: true);
 
-                    if (fail)
-                        return false;
-                }
-                else if (oneOption.Contains("!"))
-                {
-                    if (Game.Option.IsTriggered(oneOption.Replace("!", String.Empty).Trim()))
-                        return false;
-                }
-                else if (!Game.Option.IsTriggered(oneOption.Trim()))
-                {
-                    return false;
-                }
+                return !fail;
             }
-
-            return true;
+            else
+            {
+                return AvailabilityTrigger(option);
+            }
         }
     }
 }
