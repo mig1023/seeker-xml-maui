440ede98905d8ba8a1a2d317abe86cd285170863
mig1023 <mig1023@yandex.ru>
2025-03-08 14:42:19 +0300

    AvailabilityNode (aldar)

--- SeekerMAUI/Gamebook/AdventuresOfABeardlessDeceiver/Actions.cs
+++ SeekerMAUI/Gamebook/AdventuresOfABeardlessDeceiver/Actions.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Maui.Controls;
+using System;
 using System.Collections.Generic;
+using System.Runtime.Intrinsics.X86;
 
 namespace SeekerMAUI.Gamebook.AdventuresOfABeardlessDeceiver
 {
@@ -148,36 +150,18 @@ namespace SeekerMAUI.Gamebook.AdventuresOfABeardlessDeceiver
             }
         }
 
-        public override bool Availability(string option)
+        public override bool AvailabilityNode(string option)
         {
-            if (String.IsNullOrEmpty(option))
+            if (Game.Services.AvailabilityByСomparison(option))
             {
-                return true;
+                var fail = Game.Services.AvailabilityByProperty(Character.Protagonist,
+                    option, Constants.Availabilities, onlyFailTrueReturn: true);
+
+                return !fail;
             }
             else
             {
-                foreach (string oneOption in option.Split(','))
-                {
-                    if (Game.Services.AvailabilityByСomparison(oneOption))
-                    {
-                        var fail = Game.Services.AvailabilityByProperty(Character.Protagonist,
-                            oneOption, Constants.Availabilities, onlyFailTrueReturn: true);
-
-                        if (fail)
-                            return false;
-                    }
-                    else if (oneOption.Contains("!"))
-                    {
-                        if (Game.Option.IsTriggered(oneOption.Replace("!", String.Empty).Trim()))
-                            return false;
-                    }
-                    else if (!Game.Option.IsTriggered(oneOption.Trim()))
-                    {
-                        return false;
-                    }
-                }
-
-                return true;
+                return AvailabilityTrigger(option);
             }
         }
 
