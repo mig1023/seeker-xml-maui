0df47a100cd688de01843451c69388f66896293b
mig1023 <mig1023@yandex.ru>
2025-03-08 14:45:32 +0300

    AvailabilityNode (president)

--- SeekerMAUI/Gamebook/PresidentSimulator/Actions.cs
+++ SeekerMAUI/Gamebook/PresidentSimulator/Actions.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Xml;
+using Microsoft.Maui.Controls.Shapes;
 using SeekerMAUI.Game;
 
 namespace SeekerMAUI.Gamebook.PresidentSimulator
@@ -23,15 +24,43 @@ namespace SeekerMAUI.Gamebook.PresidentSimulator
             $"Отношения с СССР: {Character.Protagonist.RelationWithUSSR}",
         };
 
+        public override bool AvailabilityNode(string option)
+        {
+            if (Game.Services.AvailabilityByСomparison(option))
+            {
+                var fail = Game.Services.AvailabilityByProperty(Character.Protagonist,
+                    option, Constants.Availabilities, onlyFailTrueReturn: true);
+
+                return !fail;
+            }
+            else if (option == "СИЛЫ ВОЙСК И ПОВСТАНЦЕВ РАВНЫ")
+            {
+                return Character.Protagonist.Army == Character.Protagonist.Rebels;
+            }
+            else if (option == "СИЛЫ ВОЙСК НЕ МЕНЬШЕ СИЛЫ ПОВСТАНЦЕВ")
+            {
+                return Character.Protagonist.Army >= Character.Protagonist.Rebels;
+            }
+            else if (option == "СИЛЫ ВОЙСК МЕНЬШЕ СИЛЫ ПОВСТАНЦЕВ")
+            {
+                return Character.Protagonist.Army < Character.Protagonist.Rebels;
+            }
+            else if (option == "СИЛЫ ВОЙСК БОЛЬШЕ СИЛЫ ПОВСТАНЦЕВ")
+            {
+                int level = Services.LevelParse(option);
+                return Character.Protagonist.Army > Character.Protagonist.Rebels + level;
+            }
+            else
+            {
+                return AvailabilityTrigger(option);
+            }
+        }
+
         public override bool Availability(string option)
         {
             if (String.IsNullOrEmpty(option))
             {
                 return true;
-            }
-            else if (option.Contains("|"))
-            {
-                return option.Split('|').Where(x => Game.Option.IsTriggered(x.Trim())).Count() > 0;
             }
             else if (option.Contains(";"))
             {
@@ -44,45 +73,7 @@ namespace SeekerMAUI.Gamebook.PresidentSimulator
             }
             else
             {
-                foreach (string line in option.Split(','))
-                {
-                    if (Game.Services.AvailabilityByСomparison(line))
-                    {
-                        var fail = Game.Services.AvailabilityByProperty(Character.Protagonist,
-                            line, Constants.Availabilities, onlyFailTrueReturn: true);
-
-                        if (fail)
-                            return false;
-                    }
-                    else if (line == "СИЛЫ ВОЙСК И ПОВСТАНЦЕВ РАВНЫ")
-                    {
-                        return Character.Protagonist.Army == Character.Protagonist.Rebels;
-                    }
-                    else if (line == "СИЛЫ ВОЙСК НЕ МЕНЬШЕ СИЛЫ ПОВСТАНЦЕВ")
-                    {
-                        return Character.Protagonist.Army >= Character.Protagonist.Rebels;
-                    }
-                    else if (line == "СИЛЫ ВОЙСК МЕНЬШЕ СИЛЫ ПОВСТАНЦЕВ")
-                    {
-                        return Character.Protagonist.Army < Character.Protagonist.Rebels;
-                    }
-                    else if (line == "СИЛЫ ВОЙСК БОЛЬШЕ СИЛЫ ПОВСТАНЦЕВ")
-                    {
-                        int level = Services.LevelParse(line);
-                        return Character.Protagonist.Army > Character.Protagonist.Rebels + level;
-                    }
-                    else if (line.Contains("!"))
-                    {
-                        if (Option.IsTriggered(line.Replace("!", String.Empty).Trim()))
-                            return false;
-                    }
-                    else if (!Option.IsTriggered(line.Trim()))
-                    {
-                        return false;
-                    }
-                }
-
-                return true;
+                return base.Availability(option);
             }
         }
     }
