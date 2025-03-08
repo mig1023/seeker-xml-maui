28e00ae7389e008dc77770eae22221df10955378
mig1023 <mig1023@yandex.ru>
2025-03-08 14:46:32 +0300

    AvailabilityNode (swamp fever)

--- SeekerMAUI/Gamebook/SwampFever/Actions.cs
+++ SeekerMAUI/Gamebook/SwampFever/Actions.cs
@@ -66,30 +66,8 @@ namespace SeekerMAUI.Gamebook.SwampFever
             return !(byPrice || byUsed || byMembrane || byMucus);
         }
 
-        public override bool Availability(string option)
-        {
-            if (String.IsNullOrEmpty(option))
-            {
-                return true;
-            }
-            else
-            {
-                foreach (string oneOption in option.Split(','))
-                {
-                    if (oneOption.Contains("!"))
-                    {
-                        if (Game.Option.IsTriggered(oneOption.Replace("!", String.Empty).Trim()))
-                            return false;
-                    }
-                    else if (!Game.Option.IsTriggered(oneOption.Trim()))
-                    {
-                        return false;
-                    }
-                }
-
-                return true;
-            }
-        }
+        public override bool AvailabilityNode(string option) =>
+            AvailabilityTrigger(option);
 
         public List<string> MentalTest()
         {
