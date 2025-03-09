615ddb5b2ef6797f77121d09a1298e12e12f6656
mig1023 <mig1023@yandex.ru>
2025-03-09 12:36:50 +0300

    Options parsing fix (tachanka)

--- SeekerMAUI/Gamebook/Tachanka/Paragraphs.cs
+++ SeekerMAUI/Gamebook/Tachanka/Paragraphs.cs
@@ -6,62 +6,8 @@ namespace SeekerMAUI.Gamebook.Tachanka
 {
     class Paragraphs : Prototypes.Paragraphs, Abstract.IParagraphs
     {
-        public override Paragraph Get(int id, XmlNode xmlParagraph)
-        {
-            Paragraph paragraph = ParagraphTemplate(xmlParagraph);
-
-            foreach (XmlNode xmlOption in xmlParagraph.SelectNodes("Options/*"))
-            {
-                Option option = OptionsTemplateWithoutGoto(xmlOption);
-                option.Aftertexts.Clear();
-
-                if (ThisIsGameover(xmlOption))
-                {
-                    option.Goto = GetGoto(xmlOption);
-                }
-                else if (int.TryParse(xmlOption.Attributes["Goto"].Value, out int _))
-                {
-                    option.Goto = Xml.IntParse(xmlOption.Attributes["Goto"]);
-                }
-                else
-                {
-                    List<string> link = xmlOption.Attributes["Goto"].Value.Split(',').ToList<string>();
-                    option.Goto = int.Parse(link[random.Next(link.Count())]);
-                }
-
-                XmlNodeList optionMods = xmlOption.SelectNodes("*");
-
-                foreach (XmlNode optionMod in optionMods)
-                {
-                    if (optionMod == null)
-                    {
-                        continue;
-                    }
-                    else if (optionMod.Name == "Text")
-                    {
-                        option.Aftertexts.Add(Xml.TextLineParse(optionMod));
-                    }
-                    else if (optionMod.Name == "Image")
-                    {
-                        option.Aftertexts.Add(Xml.ImageLineParse(optionMod));
-                    }
-                    else if (optionMod.Attributes["Value"] != null)
-                    {
-                        option.Do.Add(ModificationParse(optionMod));
-                    }
-                }
-
-                paragraph.Options.Add(option);
-            }
-
-            foreach (XmlNode xmlAction in xmlParagraph.SelectNodes("Actions/*"))
-                paragraph.Actions.Add(ActionParse(xmlAction));
-
-            foreach (XmlNode xmlModification in xmlParagraph.SelectNodes("Modifications/*"))
-                paragraph.Modification.Add(ModificationParse(xmlModification));
-
-            return paragraph;
-        }
+        public override Paragraph Get(int id, XmlNode xmlParagraph) =>
+            base.Get(xmlParagraph);
 
         public override Abstract.IActions ActionParse(XmlNode xmlAction)
         {
@@ -74,6 +20,50 @@ namespace SeekerMAUI.Gamebook.Tachanka
             action.Benefit = Xml.ModificationParse(xmlAction["Benefit"], new Modification());
 
             return action;
+        }
+
+        public override Option OptionParse(XmlNode xmlOption)
+        {
+            Option option = OptionsTemplateWithoutGoto(xmlOption);
+            option.Aftertexts.Clear();
+
+            if (ThisIsGameover(xmlOption))
+            {
+                option.Goto = GetGoto(xmlOption);
+            }
+            else if (int.TryParse(xmlOption.Attributes["Goto"].Value, out int _))
+            {
+                option.Goto = Xml.IntParse(xmlOption.Attributes["Goto"]);
+            }
+            else
+            {
+                List<string> link = xmlOption.Attributes["Goto"].Value.Split(',').ToList<string>();
+                option.Goto = int.Parse(link[random.Next(link.Count())]);
+            }
+
+            XmlNodeList optionMods = xmlOption.SelectNodes("*");
+
+            foreach (XmlNode optionMod in optionMods)
+            {
+                if (optionMod == null)
+                {
+                    continue;
+                }
+                else if (optionMod.Name == "Text")
+                {
+                    option.Aftertexts.Add(Xml.TextLineParse(optionMod));
+                }
+                else if (optionMod.Name == "Image")
+                {
+                    option.Aftertexts.Add(Xml.ImageLineParse(optionMod));
+                }
+                else if (optionMod.Attributes["Value"] != null)
+                {
+                    option.Do.Add(ModificationParse(optionMod));
+                }
+            }
+
+            return option;
         }
 
         public override Abstract.IModification ModificationParse(XmlNode xmlModification) =>
