7901fd4a0c77478b8bccbdcd5debb33b71f8f887
mig1023 <mig1023@yandex.ru>
2025-04-24 17:21:37 +0300

    Character properties (souls)

--- SeekerMAUI/Gamebook/CastleOfLostSouls/Actions.cs
+++ SeekerMAUI/Gamebook/CastleOfLostSouls/Actions.cs
@@ -2,5 +2,19 @@
 
 namespace SeekerMAUI.Gamebook.CastleOfLostSouls
 {
-    class Actions : Prototypes.Actions, Abstract.IActions { }
+    class Actions : Prototypes.Actions, Abstract.IActions
+    {
+        public List<Character> Enemies { get; set; }
+
+        public override List<string> AdditionalStatus() => new List<string>
+        {
+            $"Боевая доблесть: {Character.Protagonist.Combat}",
+            $"Телосложение: {Character.Protagonist.Constitution}",
+            $"Сообразительность: {Character.Protagonist.Ingenuity}",
+            $"Магическая стойкость: {Character.Protagonist.Resistence}",
+            $"Честь: {Character.Protagonist.Honor}",
+            $"Доспехи: {Character.Protagonist.Armor}",
+            $"Золото: {Character.Protagonist.Gold}",
+        };
+    }
 }
--- SeekerMAUI/Gamebook/CastleOfLostSouls/Character.cs
+++ SeekerMAUI/Gamebook/CastleOfLostSouls/Character.cs
@@ -2,5 +2,111 @@
 
 namespace SeekerMAUI.Gamebook.CastleOfLostSouls
 {
-    class Character : Prototypes.Character, Abstract.ICharacter { }
+    class Character : Prototypes.Character, Abstract.ICharacter
+    {
+        public static Character Protagonist { get; set; }
+        public override void Set(object character) =>
+            Protagonist = (Character)character;
+
+        private int _combat;
+        public int Combat
+        {
+            get => _combat;
+            set => _combat = Game.Param.Setter(value);
+        }
+
+        private int _constitution;
+        public int Constitution
+        {
+            get => _constitution;
+            set => _constitution = Game.Param.Setter(value);
+        }
+
+        private int _ingenuity;
+        public int Ingenuity
+        {
+            get => _ingenuity;
+            set => _ingenuity = Game.Param.Setter(value);
+        }
+
+        private int _resistence;
+        public int Resistence
+        {
+            get => _resistence;
+            set => _resistence = Game.Param.Setter(value);
+        }
+
+        private int _honor;
+        public int Honor
+        {
+            get => _honor;
+            set => _honor = Game.Param.Setter(value);
+        }
+
+        private int _armor;
+        public int Armor
+        {
+            get => _armor;
+            set => _armor = Game.Param.Setter(value);
+        }
+
+        private int _gold;
+        public int Gold
+        {
+            get => _gold;
+            set => _gold = Game.Param.Setter(value, _gold, this);
+        }
+
+        public override void Init()
+        {
+            base.Init();
+
+            Combat = Game.Dice.Roll() + 5;
+            Constitution = Game.Dice.Roll(dices: 2) + 10;
+            Ingenuity = Game.Dice.Roll() + 5;
+            Honor = 3;
+            Armor = 2;
+            Gold = 10;
+
+            Resistence = Game.Dice.Roll(dices: 2) + 3;
+
+            if (Combat > 6)
+                Resistence += 1;
+
+            if (Constitution < 15)
+                Resistence += 1;
+        }
+
+        public Character Clone() => new Character()
+        {
+            IsProtagonist = this.IsProtagonist,
+            Name = this.Name,
+            Combat = this.Combat,
+            Constitution = this.Constitution,
+            Ingenuity = this.Ingenuity,
+            Honor = this.Honor,
+            Armor = this.Armor,
+            Gold = this.Gold,
+            Resistence = this.Resistence,
+        };
+
+        public override string Save() => String.Join("|",
+            Combat, Constitution, Ingenuity, Honor, Armor, Gold, Resistence
+        );
+
+        public override void Load(string saveLine)
+        {
+            string[] save = saveLine.Split('|');
+
+            Combat = int.Parse(save[0]);
+            Constitution = int.Parse(save[1]);
+            Ingenuity = int.Parse(save[2]);
+            Honor = int.Parse(save[3]);
+            Armor = int.Parse(save[4]);
+            Gold = int.Parse(save[5]);
+            Resistence = int.Parse(save[6]);
+
+            IsProtagonist = true;
+        }
+    }
 }
