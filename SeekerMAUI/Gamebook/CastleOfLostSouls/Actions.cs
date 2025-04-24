400d31fc4f8b8474a83cc773b2ef433ad5ed8370
mig1023 <mig1023@yandex.ru>
2025-04-24 17:22:13 +0300

    Status <--> AdditionalStatus (souls)

--- SeekerMAUI/Gamebook/CastleOfLostSouls/Actions.cs
+++ SeekerMAUI/Gamebook/CastleOfLostSouls/Actions.cs
@@ -6,15 +6,19 @@ namespace SeekerMAUI.Gamebook.CastleOfLostSouls
     {
         public List<Character> Enemies { get; set; }
 
+        public override List<string> Status() => new List<string>
+        {
+            $"Честь: {Character.Protagonist.Honor}",
+            $"Доспехи: {Character.Protagonist.Armor}",
+            $"Золото: {Character.Protagonist.Gold}",
+        };
+
         public override List<string> AdditionalStatus() => new List<string>
         {
             $"Боевая доблесть: {Character.Protagonist.Combat}",
             $"Телосложение: {Character.Protagonist.Constitution}",
             $"Сообразительность: {Character.Protagonist.Ingenuity}",
             $"Магическая стойкость: {Character.Protagonist.Resistence}",
-            $"Честь: {Character.Protagonist.Honor}",
-            $"Доспехи: {Character.Protagonist.Armor}",
-            $"Золото: {Character.Protagonist.Gold}",
         };
     }
 }
