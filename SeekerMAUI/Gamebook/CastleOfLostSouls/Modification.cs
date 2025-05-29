0527999c50b3a1e7c0644cadadf521c0e308583d
mig1023 <mig1023@yandex.ru>
2025-05-29 12:28:37 +0300

    'Salakar' modification (souls)

--- SeekerMAUI/Gamebook/CastleOfLostSouls/Modification.cs
+++ SeekerMAUI/Gamebook/CastleOfLostSouls/Modification.cs
@@ -4,7 +4,22 @@ namespace SeekerMAUI.Gamebook.CastleOfLostSouls
 {
     class Modification : Prototypes.Modification, Abstract.IModification
     {
-        public override void Do() =>
-            base.Do(Character.Protagonist);
+        public override void Do()
+        {
+            if (Name == "Salakar")
+            {
+                Character.Protagonist.Combat = 8;
+                Character.Protagonist.Constitution = 11;
+                Character.Protagonist.Ingenuity = 7;
+                Character.Protagonist.Resistence = 5;
+                Character.Protagonist.Honor = 0;
+                Character.Protagonist.Armour = 2;
+                Character.Protagonist.Gold = 12;
+            }
+            else
+            {
+                base.Do(Character.Protagonist);
+            }
+        }
     }
 }
