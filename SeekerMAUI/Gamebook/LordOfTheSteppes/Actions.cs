1c1573868e087e163781ba96dc28c9d7eb4f4a95
mig1023 <mig1023@yandex.ru>
2025-07-03 14:06:40 +0300

    Classes ltl refactoring (lord)

--- SeekerMAUI/Gamebook/LordOfTheSteppes/Actions.cs
+++ SeekerMAUI/Gamebook/LordOfTheSteppes/Actions.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace SeekerMAUI.Gamebook.LordOfTheSteppes
 {
@@ -271,78 +269,11 @@ namespace SeekerMAUI.Gamebook.LordOfTheSteppes
             }
         }
 
-        public List<string> GameOfDice()
-        {
-            if (Character.Protagonist.Coins < 5)
-                return new List<string> { "BAD|У вас не достаточно денег, чтобы играть..." };
+        public List<string> GameOfDice() =>
+            Dice.Games();
 
-            List<string> diceGame = new List<string> { };
-
-            int myResult, enemyResult;
-
-            do
-            {
-                Game.Dice.DoubleRoll(out int hisFirstDice, out int hisSecondDice);
-                enemyResult = hisFirstDice + hisSecondDice + 4;
-
-                diceGame.Add($"Он бросил: " +
-                    $"{Game.Dice.Symbol(hisFirstDice)} + " +
-                    $"{Game.Dice.Symbol(hisSecondDice)} = {enemyResult}");
-
-                Game.Dice.DoubleRoll(out int firstDice, out int secondDice);
-                myResult = firstDice + secondDice;
-
-                diceGame.Add($"Вы бросили: " +
-                    $"{Game.Dice.Symbol(firstDice)} + " +
-                    $"{Game.Dice.Symbol(secondDice)} = {myResult}");
-
-                diceGame.Add(String.Empty);
-            }
-            while (myResult == enemyResult);
-
-            if (myResult > enemyResult)
-            {
-                diceGame.Add("BIG|GOOD|ВЫ ВЫИГРАЛИ 5 МОНЕТ:)");
-                Character.Protagonist.Coins += 5;
-            }
-            else
-            {
-                diceGame.Add("BIG|BAD|ПРОИГРАЛИ 5 МОНЕТ :(");
-                Character.Protagonist.Coins -= 5;
-            }
-
-            return diceGame;
-        }
-
-        public List<string> DiceCheck()
-        {
-            List<string> diceCheck = new List<string> { };
-
-            int firstDice = Game.Dice.Roll();
-            int dicesResult = firstDice;
-
-            string size = (Odd ? String.Empty : "BIG|");
-
-            if (Dices == 1)
-            {
-                diceCheck.Add($"{size}На кубикe выпало: {Game.Dice.Symbol(firstDice)}");
-            }
-            else
-            {
-                int secondDice = Game.Dice.Roll();
-                dicesResult += secondDice + (Initiative ? Character.Protagonist.Initiative : 0);
-                string initLine = (Initiative ? $" + {Character.Protagonist.Initiative} Инициатива" : String.Empty);
-
-                diceCheck.Add($"{size}На кубиках выпало: " +
-                    $"{Game.Dice.Symbol(firstDice)} + " +
-                    $"{Game.Dice.Symbol(secondDice)}{initLine} = {dicesResult}");
-            }
-
-            if (Odd)
-                diceCheck.Add(dicesResult % 2 == 0 ? "BIG|ЧЁТНОЕ ЧИСЛО!" : "BIG|НЕЧЁТНОЕ ЧИСЛО!");
-
-            return diceCheck;
-        }
+        public List<string> DiceCheck() =>
+            Dice.Check(Odd, Dices, Initiative);
 
         public List<string> Fight()
         {
---
+++ SeekerMAUI/Gamebook/LordOfTheSteppes/Dice.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace SeekerMAUI.Gamebook.LordOfTheSteppes
+{
+    class Dice
+    {
+        public static List<string> Games()
+        {
+            if (Character.Protagonist.Coins < 5)
+                return new List<string> { "BAD|У вас не достаточно денег, чтобы играть..." };
+
+            List<string> diceGame = new List<string> { };
+
+            int myResult, enemyResult;
+
+            do
+            {
+                Game.Dice.DoubleRoll(out int hisFirstDice, out int hisSecondDice);
+                enemyResult = hisFirstDice + hisSecondDice + 4;
+
+                diceGame.Add($"Он бросил: " +
+                    $"{Game.Dice.Symbol(hisFirstDice)} + " +
+                    $"{Game.Dice.Symbol(hisSecondDice)} = {enemyResult}");
+
+                Game.Dice.DoubleRoll(out int firstDice, out int secondDice);
+                myResult = firstDice + secondDice;
+
+                diceGame.Add($"Вы бросили: " +
+                    $"{Game.Dice.Symbol(firstDice)} + " +
+                    $"{Game.Dice.Symbol(secondDice)} = {myResult}");
+
+                diceGame.Add(String.Empty);
+            }
+            while (myResult == enemyResult);
+
+            if (myResult > enemyResult)
+            {
+                diceGame.Add("BIG|GOOD|ВЫ ВЫИГРАЛИ 5 МОНЕТ:)");
+                Character.Protagonist.Coins += 5;
+            }
+            else
+            {
+                diceGame.Add("BIG|BAD|ПРОИГРАЛИ 5 МОНЕТ :(");
+                Character.Protagonist.Coins -= 5;
+            }
+
+            return diceGame;
+        }
+
+        public static List<string> Check(bool odd, int dices, bool initiative)
+        {
+            List<string> diceCheck = new List<string> { };
+
+            int firstDice = Game.Dice.Roll();
+            int dicesResult = firstDice;
+
+            string size = (odd ? String.Empty : "BIG|");
+
+            if (dices == 1)
+            {
+                diceCheck.Add($"{size}На кубикe выпало: {Game.Dice.Symbol(firstDice)}");
+            }
+            else
+            {
+                int secondDice = Game.Dice.Roll();
+                dicesResult += secondDice + (initiative ? Character.Protagonist.Initiative : 0);
+                string initLine = (initiative ? $" + {Character.Protagonist.Initiative} Инициатива" : String.Empty);
+
+                diceCheck.Add($"{size}На кубиках выпало: " +
+                    $"{Game.Dice.Symbol(firstDice)} + " +
+                    $"{Game.Dice.Symbol(secondDice)}{initLine} = {dicesResult}");
+            }
+
+            if (odd)
+                diceCheck.Add(dicesResult % 2 == 0 ? "BIG|ЧЁТНОЕ ЧИСЛО!" : "BIG|НЕЧЁТНОЕ ЧИСЛО!");
+
+            return diceCheck;
+        }
+    }
+}
