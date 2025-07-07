02a914341f50280eeb5ab4b50645cb8b96eb02eb
mig1023 <mig1023@yandex.ru>
2025-07-07 12:02:25 +0300

    Classes ltl refactoring (string)

--- SeekerMAUI/Gamebook/StringOfWorlds/Actions.cs
+++ SeekerMAUI/Gamebook/StringOfWorlds/Actions.cs
@@ -195,56 +195,11 @@ namespace SeekerMAUI.Gamebook.StringOfWorlds
             return luckCheck;
         }
 
-        public List<string> DiceWounds()
-        {
-            List<string> diceCheck = new List<string> { };
+        public List<string> DiceWounds() =>
+            Dices.Wounds();
 
-            int dices = 0;
-
-            for (int i = 1; i <= 2; i++)
-            {
-                int dice = Game.Dice.Roll();
-                dices += dice;
-                diceCheck.Add($"На {i} выпало: {Game.Dice.Symbol(dice)}");
-            }
-
-            Character.Protagonist.Strength -= dices;
-
-            diceCheck.Add($"BIG|BAD|Вы потеряли жизней: {dices}");
-
-            return diceCheck;
-        }
-        
-        public List<string> GameOfDice()
-        {
-            List<string> diceGame = new List<string> { };
-
-            int myResult, enemyResult;
-
-            do
-            {
-                Game.Dice.DoubleRoll(out int firstDice, out int secondDice);
-                myResult = firstDice + secondDice;
-
-                diceGame.Add($"Вы бросили: " +
-                    $"{Game.Dice.Symbol(firstDice)} + " +
-                    $"{Game.Dice.Symbol(secondDice)} = {myResult}");
-
-                Game.Dice.DoubleRoll(out int hisFirstDice, out int hisSecondDice);
-                enemyResult = hisFirstDice + hisSecondDice;
-
-                diceGame.Add($"Он бросил: " +
-                    $"{Game.Dice.Symbol(hisFirstDice)} + " +
-                    $"{Game.Dice.Symbol(hisSecondDice)} = {enemyResult}");
-
-                diceGame.Add(String.Empty);
-            }
-            while (myResult == enemyResult);
-
-            diceGame.Add(Result(myResult > enemyResult, "ВЫИГРАЛИ", "ПРОИГРАЛИ"));
-
-            return diceGame;
-        }
+        public List<string> GameOfDice() =>
+            Dices.Games(this);
 
         public List<string> Break()
         {
---
+++ SeekerMAUI/Gamebook/StringOfWorlds/Dices.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace SeekerMAUI.Gamebook.StringOfWorlds
+{
+    class Dices
+    {
+        public static List<string> Wounds()
+        {
+            List<string> diceCheck = new List<string> { };
+
+            int dices = 0;
+
+            for (int i = 1; i <= 2; i++)
+            {
+                int dice = Game.Dice.Roll();
+                dices += dice;
+                diceCheck.Add($"На {i} выпало: {Game.Dice.Symbol(dice)}");
+            }
+
+            Character.Protagonist.Strength -= dices;
+
+            diceCheck.Add($"BIG|BAD|Вы потеряли жизней: {dices}");
+
+            return diceCheck;
+        }
+
+        public static List<string> Games(Actions actions)
+        {
+            List<string> diceGame = new List<string> { };
+
+            int myResult, enemyResult;
+
+            do
+            {
+                Game.Dice.DoubleRoll(out int firstDice, out int secondDice);
+                myResult = firstDice + secondDice;
+
+                diceGame.Add($"Вы бросили: " +
+                    $"{Game.Dice.Symbol(firstDice)} + " +
+                    $"{Game.Dice.Symbol(secondDice)} = {myResult}");
+
+                Game.Dice.DoubleRoll(out int hisFirstDice, out int hisSecondDice);
+                enemyResult = hisFirstDice + hisSecondDice;
+
+                diceGame.Add($"Он бросил: " +
+                    $"{Game.Dice.Symbol(hisFirstDice)} + " +
+                    $"{Game.Dice.Symbol(hisSecondDice)} = {enemyResult}");
+
+                diceGame.Add(String.Empty);
+            }
+            while (myResult == enemyResult);
+
+            diceGame.Add(actions.Result(myResult > enemyResult, "ВЫИГРАЛИ", "ПРОИГРАЛИ"));
+
+            return diceGame;
+        }
+    }
+}
