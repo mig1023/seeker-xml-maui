7c770d5ee40d674990308aa3a4f526a2d717b671
mig1023 <mig1023@yandex.ru>
2025-07-07 12:01:51 +0300

    Classes ltl refactoring (silver age)

--- SeekerMAUI/Gamebook/SilverAgeSilhouette/Actions.cs
+++ SeekerMAUI/Gamebook/SilverAgeSilhouette/Actions.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace SeekerMAUI.Gamebook.SilverAgeSilhouette
@@ -18,19 +16,19 @@ namespace SeekerMAUI.Gamebook.SilverAgeSilhouette
             }
             else if (option == "Нет издания или неудачник")
             {
-                return AvailabilitySpecialTrigger();
+                return Availabilities.SpecialTrigger();
             }
             else if (option.Contains("||"))
             {
-                return AvailabilityExclusiveTrigger(option);
+                return Availabilities.ExclusiveTrigger(option);
             }
             else if (option.Contains("|"))
             {
-                return AvailabilityMultiplesTrigger(option);
+                return Availabilities.MultiplesTrigger(option);
             }
             else if (option.Contains("ОЦЕНКА"))
             {
-                return AvailabilityRating(option);
+                return Availabilities.Rating(option);
             }
             else
             {
@@ -38,64 +36,6 @@ namespace SeekerMAUI.Gamebook.SilverAgeSilhouette
             }
         }
 
-        private bool AvailabilityRating(string option)
-        {
-            bool logic = option.Contains("!");
-
-            List<string> ratings = option
-                .Replace("!", String.Empty)
-                .Split(',')
-                .Select(x => x.Trim())
-                .ToList();
-
-            foreach (string rating in ratings)
-            {
-                int level = Game.Services.LevelParse(rating);
-
-                if (rating.Contains("ОЦЕНКА >=") && (level > Character.Protagonist.Rating))
-                    return logic;
-
-                if (rating.Contains("ОЦЕНКА <") && (level <= Character.Protagonist.Rating))
-                    return logic;
-            }
-
-            return !logic;
-        }
-
-        private bool AvailabilityMultiplesTrigger(string option)
-        {
-            bool triggers = option
-                .Split('|')
-                .Where(x => Game.Option.IsTriggered(x.Trim()))
-                .Count() > 0;
-
-            return triggers;
-        }
         
-        private bool AvailabilityExclusiveTrigger(string option)
-        {
-            List<string> triggers = option
-                .Split(new string[] { "||" }, StringSplitOptions.RemoveEmptyEntries)
-                .Select(x => x.Trim())
-                .ToList();
-
-            return Game.Option.IsTriggered(triggers[0]) && !Game.Option.IsTriggered(triggers[1]);
-        }
-
-        private bool AvailabilitySpecialTrigger()
-        {
-            if (!Game.Option.IsTriggered("Собственное издание"))
-            {
-                return true;
-            }
-            else if (Game.Option.IsTriggered("Неудачник"))
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
     }
 }
---
+++ SeekerMAUI/Gamebook/SilverAgeSilhouette/Availabilities.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace SeekerMAUI.Gamebook.SilverAgeSilhouette
+{
+    class Availabilities
+    {
+        public static bool Rating(string option)
+        {
+            bool logic = option.Contains("!");
+
+            List<string> ratings = option
+                .Replace("!", String.Empty)
+                .Split(',')
+                .Select(x => x.Trim())
+                .ToList();
+
+            foreach (string rating in ratings)
+            {
+                int level = Game.Services.LevelParse(rating);
+
+                if (rating.Contains("ОЦЕНКА >=") && (level > Character.Protagonist.Rating))
+                    return logic;
+
+                if (rating.Contains("ОЦЕНКА <") && (level <= Character.Protagonist.Rating))
+                    return logic;
+            }
+
+            return !logic;
+        }
+
+        public static bool MultiplesTrigger(string option)
+        {
+            bool triggers = option
+                .Split('|')
+                .Where(x => Game.Option.IsTriggered(x.Trim()))
+                .Count() > 0;
+
+            return triggers;
+        }
+
+        public static bool ExclusiveTrigger(string option)
+        {
+            List<string> triggers = option
+                .Split(new string[] { "||" }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => x.Trim())
+                .ToList();
+
+            return Game.Option.IsTriggered(triggers[0]) && !Game.Option.IsTriggered(triggers[1]);
+        }
+
+        public static bool SpecialTrigger()
+        {
+            if (!Game.Option.IsTriggered("Собственное издание"))
+            {
+                return true;
+            }
+            else if (Game.Option.IsTriggered("Неудачник"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
