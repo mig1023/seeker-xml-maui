8e066a9685097a3600554a039be13e4499f5f29f
mig1023 <mig1023@yandex.ru>
2025-07-07 12:02:33 +0300

    Classes ltl refactoring (tremble)

--- SeekerMAUI/Gamebook/Tremble/Actions.cs
+++ SeekerMAUI/Gamebook/Tremble/Actions.cs
@@ -1,5 +1,4 @@
-﻿using SeekerMAUI.Game;
-using System;
+﻿using System;
 
 namespace SeekerMAUI.Gamebook.Tremble
 {
@@ -110,33 +109,11 @@ namespace SeekerMAUI.Gamebook.Tremble
             }
         }
 
-        public List<string> GoodLuck()
-        {
-            Game.Dice.DoubleRoll(out int firstDice, out int secondDice);
-            var goodLuck = (firstDice + secondDice) < Character.Protagonist.Luck;
-            var luckLine = goodLuck ? "<=" : ">";
+        public List<string> GoodLuck() =>
+            Dices.Luck();
 
-            List<string> luckCheck = new List<string> {
-                $"Проверка удачи: {Game.Dice.Symbol(firstDice)} + " +
-                $"{Game.Dice.Symbol(secondDice)} {luckLine} {Character.Protagonist.Luck}" };
-
-            if (firstDice == secondDice)
-            {
-                luckCheck.Add("На кубиках выпал дубль - это всегда удача!");
-                goodLuck = true;
-            }
-
-            luckCheck.Add(goodLuck ? "BIG|GOOD|УСПЕХ :)" : "BIG|BAD|НЕУДАЧА :(");
-            Game.Buttons.Disable(goodLuck, "Win", "Fail");
-
-            if (Character.Protagonist.Luck > 1)
-            {
-                Character.Protagonist.Luck -= 1;
-                luckCheck.Add("Уровень удачи снижен на единицу");
-            }
-
-            return luckCheck;
-        }
+        public List<string> DiceCheck() =>
+            Dices.Check(WoundsByDice, HeavyDamageByDice, LightDamageByDice);
 
         public List<string> Fight()
         {
@@ -200,54 +177,6 @@ namespace SeekerMAUI.Gamebook.Tremble
 
                 round += 1;
             }
-        }
-
-        public List<string> DiceCheck()
-        {
-            var dice = Dice.Roll();
-            var check = new List<string> { $"BIG|Кубик: {Dice.Symbol(dice)}" };
-
-            if (WoundsByDice)
-            {
-                Character.Protagonist.Endurance -= dice;
-                var line = Game.Services.CoinsNoun(dice, "единицу", "единицы", "единиц");
-                check.Add($"BAD|BOLD|Вы потеряли {dice} {line} Выносливости");
-
-                if (Character.Protagonist.Endurance <= 0)
-                {
-                    Character.Protagonist.Endurance = 1;
-                    check.Add("1 единица Выносливости всё-таки остаётся!");
-                }
-            }
-            else if (HeavyDamageByDice)
-            {
-                if (dice > 3)
-                {
-                    Character.Protagonist.Endurance -= 4;
-                    Character.Protagonist.Skill -= 1;
-                    check.Add("BAD|BOLD|На кубике выпало 4+!");
-                    check.Add("Вы теряете 3 единицы Выносливости и 1 единицу Ловкости!");
-                }
-                else
-                {
-                    check.Add("GOOD|BOLD|Обошлось..!");
-                }
-            }
-            else if (LightDamageByDice)
-            {
-                if (dice <= 2)
-                {
-                    Character.Protagonist.Endurance -= 1;
-                    check.Add("BAD|BOLD|На кубике выпало меньше 3!");
-                    check.Add("Вы теряете 1 единицу Выносливости!");
-                }
-                else
-                {
-                    check.Add("GOOD|BOLD|Обошлось..!");
-                }
-            }
-
-            return check;
         }
     }
 }
---
+++ SeekerMAUI/Gamebook/Tremble/Dices.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace SeekerMAUI.Gamebook.Tremble
+{
+    class Dices
+    {
+        public static List<string> Luck()
+        {
+            Game.Dice.DoubleRoll(out int firstDice, out int secondDice);
+            var goodLuck = (firstDice + secondDice) < Character.Protagonist.Luck;
+            var luckLine = goodLuck ? "<=" : ">";
+
+            List<string> luckCheck = new List<string> {
+                $"Проверка удачи: {Game.Dice.Symbol(firstDice)} + " +
+                $"{Game.Dice.Symbol(secondDice)} {luckLine} {Character.Protagonist.Luck}" };
+
+            if (firstDice == secondDice)
+            {
+                luckCheck.Add("На кубиках выпал дубль - это всегда удача!");
+                goodLuck = true;
+            }
+
+            luckCheck.Add(goodLuck ? "BIG|GOOD|УСПЕХ :)" : "BIG|BAD|НЕУДАЧА :(");
+            Game.Buttons.Disable(goodLuck, "Win", "Fail");
+
+            if (Character.Protagonist.Luck > 1)
+            {
+                Character.Protagonist.Luck -= 1;
+                luckCheck.Add("Уровень удачи снижен на единицу");
+            }
+
+            return luckCheck;
+        }
+
+        public static List<string> Check(bool woundsByDice,
+            bool heavyDamageByDice, bool lightDamageByDice)
+        {
+            var dice = Game.Dice.Roll();
+            var check = new List<string> { $"BIG|Кубик: {Game.Dice.Symbol(dice)}" };
+
+            if (woundsByDice)
+            {
+                Character.Protagonist.Endurance -= dice;
+                var line = Game.Services.CoinsNoun(dice, "единицу", "единицы", "единиц");
+                check.Add($"BAD|BOLD|Вы потеряли {dice} {line} Выносливости");
+
+                if (Character.Protagonist.Endurance <= 0)
+                {
+                    Character.Protagonist.Endurance = 1;
+                    check.Add("1 единица Выносливости всё-таки остаётся!");
+                }
+            }
+            else if (heavyDamageByDice)
+            {
+                if (dice > 3)
+                {
+                    Character.Protagonist.Endurance -= 4;
+                    Character.Protagonist.Skill -= 1;
+                    check.Add("BAD|BOLD|На кубике выпало 4+!");
+                    check.Add("Вы теряете 3 единицы Выносливости и 1 единицу Ловкости!");
+                }
+                else
+                {
+                    check.Add("GOOD|BOLD|Обошлось..!");
+                }
+            }
+            else if (lightDamageByDice)
+            {
+                if (dice <= 2)
+                {
+                    Character.Protagonist.Endurance -= 1;
+                    check.Add("BAD|BOLD|На кубике выпало меньше 3!");
+                    check.Add("Вы теряете 1 единицу Выносливости!");
+                }
+                else
+                {
+                    check.Add("GOOD|BOLD|Обошлось..!");
+                }
+            }
+
+            return check;
+        }
+    }
+}
