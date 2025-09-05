20adac097220e8d62aa0138596b01d9effad090f
mig1023 <mig1023@yandex.ru>
2025-09-05 11:11:00 +0300

    Loaylty action (silver age)

--- SeekerMAUI/Gamebook/SilverAgeSilhouette/Actions.cs
+++ SeekerMAUI/Gamebook/SilverAgeSilhouette/Actions.cs
@@ -8,6 +8,51 @@ namespace SeekerMAUI.Gamebook.SilverAgeSilhouette
         public List<string> Verse() =>
             Character.Protagonist.Verse.Select(x => Regex.Unescape(x)).ToList();
 
+        public List<string> Loyalty()
+        {
+            var results = new List<string> { "BOLD|CЧИТАЕМ:" };
+            var result = 0;
+
+            foreach (string add in Constants.AddLoyalty)
+            {
+                if (Game.Option.IsTriggered(add))
+                {
+                    results.Add($"GOOD|+1 за «{add}»");
+                    result += 1;
+                }
+            }
+
+            foreach (string sub in Constants.SubLoyalty)
+            {
+                if (Game.Option.IsTriggered(sub))
+                {
+                    results.Add($"BAD|-1 за «{sub}»");
+                    result -= 1;
+                }
+            }
+
+            if (results.Count == 0)
+            {
+                results.Add("BAD|Даже, как-то, и вспомнить нечего...");
+            }
+
+            var line = result < 0 ? "минус " : string.Empty;
+            results.Add($"BIG|BOLD|ИТОГО: {line}{result}");
+
+            if (result > 0)
+            {
+                results.Add("Это положительное число.");
+                Game.Buttons.Disable("Отрицательное");
+            }
+            else
+            {
+                results.Add("Это " + (result == 0 ? "полный ноль..." : "отрицательное число."));
+                Game.Buttons.Disable("Положительное");
+            }
+
+            return results;
+        }
+
         public override bool Availability(string option)
         {
             if (String.IsNullOrEmpty(option))
--- SeekerMAUI/Gamebook/SilverAgeSilhouette/Constants.cs
+++ SeekerMAUI/Gamebook/SilverAgeSilhouette/Constants.cs
@@ -2,5 +2,10 @@
 
 namespace SeekerMAUI.Gamebook.SilverAgeSilhouette
 {
-    class Constants : Prototypes.Constants, Abstract.IConstants { }
+    class Constants : Prototypes.Constants, Abstract.IConstants
+    {
+        public static List<string> AddLoyalty { get; set; }
+
+        public static List<string> SubLoyalty { get; set; }
+    }
 }
