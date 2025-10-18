2fa201ce0c8086c7a405f678caa20d54acafc7da
mig1023 <mig1023@yandex.ru>
2025-10-18 14:52:48 +0300

    statuses + parsing (knight)

--- SeekerMAUI/Gamebook/KnightOfTheLivingDead/Actions.cs
+++ SeekerMAUI/Gamebook/KnightOfTheLivingDead/Actions.cs
@@ -2,5 +2,32 @@
 
 namespace SeekerMAUI.Gamebook.KnightOfTheLivingDead
 {
-    class Actions : Prototypes.Actions, Abstract.IActions { }
+    class Actions : Prototypes.Actions, Abstract.IActions
+    {
+        public List<Character> Enemies { get; set; }
+
+        public override List<string> Status() => new List<string>
+        {
+            $"Оружие: {Character.Protagonist.Weapon}",
+            $"Атака: {Character.Protagonist.Attack}",
+            $"Повреждения: {Character.Protagonist.Damage}",
+            $"Очки нежизни: {Character.Protagonist.Hitpoints}",
+        };
+
+        public override bool GameOver(out int toEndParagraph, out string toEndText) =>
+            GameOverBy(Character.Protagonist.Hitpoints, out toEndParagraph, out toEndText);
+
+        public override List<string> Representer()
+        {
+            List<string> enemies = new List<string>();
+
+            if (Enemies == null)
+                return enemies;
+
+            foreach (Character enemy in Enemies)
+                enemies.Add($"{enemy.Name}\nатака {enemy.Attack}  повреждения {enemy.Damage}  очки нежизни {enemy.Hitpoints}");
+
+            return enemies;
+        }
+    }
 }
--- SeekerMAUI/Gamebook/KnightOfTheLivingDead/Paragraphs.cs
+++ SeekerMAUI/Gamebook/KnightOfTheLivingDead/Paragraphs.cs
@@ -5,7 +5,44 @@ namespace SeekerMAUI.Gamebook.KnightOfTheLivingDead
 {
     class Paragraphs : Prototypes.Paragraphs, Abstract.IParagraphs
     {
-        public override Paragraph Get(int id, XmlNode xmlParagraph) =>
-            base.Get(xmlParagraph);
+        public override Paragraph Get(int id, XmlNode xmlParagraph)
+        {
+            Paragraph paragraph = ParagraphTemplate(xmlParagraph);
+
+            foreach (XmlNode xmlOption in xmlParagraph.SelectNodes("Options/*"))
+            {
+                Option option = OptionsTemplateWithoutGoto(xmlOption);
+
+                if (ThisIsGameover(xmlOption) || ThisIsBack(xmlOption))
+                {
+                    option.Goto = GetGoto(xmlOption, wayBack: Character.Protagonist.WayBack);
+                }
+                else
+                {
+                    option.Goto = Xml.IntParse(xmlOption.Attributes["Goto"]);
+                }
+
+                XmlNode optionMod = xmlOption.SelectSingleNode("*");
+
+                if ((optionMod != null) && (optionMod["Value"] != null))
+                    option.Do.Add(Xml.ModificationParse(optionMod, new Modification()));
+
+                paragraph.Options.Add(option);
+            }
+
+            foreach (XmlNode xmlAction in xmlParagraph.SelectNodes("Actions/*"))
+                paragraph.Actions.Add(ActionParse(xmlAction));
+
+            foreach (XmlNode xmlModification in xmlParagraph.SelectNodes("Modifications/*"))
+                paragraph.Modification.Add(Xml.ModificationParse(xmlModification, new Modification()));
+
+            return paragraph;
+        }
+
+        public override Option OptionParse(XmlNode xmlOption) =>
+            OptionParseWithDo(xmlOption, new Modification());
+
+        public override Abstract.IModification ModificationParse(XmlNode xmlModification) =>
+            Xml.ModificationParse(xmlModification, new Modification());
     }
 }
