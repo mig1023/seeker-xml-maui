1c74fd07aeb2a11db3db720a8c1f208df951c94d
mig1023 <mig1023@yandex.ru>
2025-12-18 16:32:14 +0300

    AddStatuses (sherlock)

--- SeekerMAUI/Gamebook/SherlockHolmes/Actions.cs
+++ SeekerMAUI/Gamebook/SherlockHolmes/Actions.cs
@@ -2,5 +2,16 @@
 
 namespace SeekerMAUI.Gamebook.SherlockHolmes
 {
-    class Actions : Prototypes.Actions, Abstract.IActions { }
+    class Actions : Prototypes.Actions, Abstract.IActions
+    {
+        public override List<string> AdditionalStatus() => new List<string>
+        {
+            $"Ловкость: {Character.Protagonist.Dexterity}",
+            $"Изобретательность: {Character.Protagonist.Ingenuity}",
+            $"Интуиция: {Character.Protagonist.Intuition}",
+            $"Красноречие: {Character.Protagonist.Eloquence}",
+            $"Наблюдательность: {Character.Protagonist.Observation}",
+            $"Эрудиция: {Character.Protagonist.Erudition}",
+        };
+    }
 }
