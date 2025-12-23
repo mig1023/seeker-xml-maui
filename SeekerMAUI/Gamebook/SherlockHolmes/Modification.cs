2110592103edcaaa736015fbd8626c82c9f13068
mig1023 <mig1023@yandex.ru>
2025-12-23 13:04:17 +0300

    ReGameCleaning mods (sherlock)

--- SeekerMAUI/Gamebook/SherlockHolmes/Modification.cs
+++ SeekerMAUI/Gamebook/SherlockHolmes/Modification.cs
@@ -4,7 +4,17 @@ namespace SeekerMAUI.Gamebook.SherlockHolmes
 {
     class Modification : Prototypes.Modification, Abstract.IModification
     {
-        public override void Do() =>
-            base.Do(Character.Protagonist);
+        public override void Do()
+        {
+            if (Name == "ReGameCleaning")
+            {
+                Character.Protagonist.EvidenceCount = 0;
+                Game.Data.Clean(reStart: true);
+            }
+            else
+            {
+                base.Do(Character.Protagonist);
+            }
+        }
     }
 }
