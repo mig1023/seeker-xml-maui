40c59bee098a581147853d49ee96333a4561e5a1
mig1023 <mig1023@yandex.ru>
2026-02-26 16:56:44 +0300

    'Set' mod type (grace)

--- SeekerMAUI/Gamebook/NoYourGrace/Modification.cs
+++ SeekerMAUI/Gamebook/NoYourGrace/Modification.cs
@@ -6,7 +6,19 @@ namespace SeekerMAUI.Gamebook.NoYourGrace
     {
         public override void Do()
         {
-            if (Constants.Availabilities.ContainsKey(Name))
+            if (Name == "Set")
+            {
+                var values = ValueString
+                    .Split(',')
+                    .Select(x => x.Trim())
+                    .ToList();
+
+                var property = values[0];
+                var value = int.Parse(values[1]);
+
+                SetProperty(Character.Protagonist, property, value);
+            }
+            else if (Constants.Availabilities.ContainsKey(Name))
             {
                 int currentValue = GetProperty(Character.Protagonist, Constants.Availabilities[Name]);
                 SetProperty(Character.Protagonist, Constants.Availabilities[Name], currentValue + Value);
