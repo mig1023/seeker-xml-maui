08b8744eb2573274cabdb9bd8fa1072ea26e7d88
mig1023 <mig1023@yandex.ru>
2026-03-08 16:37:30 +0300

    Victory Points action (grace)

--- SeekerMAUI/Gamebook/NoYourGrace/Actions.cs
+++ SeekerMAUI/Gamebook/NoYourGrace/Actions.cs
@@ -44,5 +44,45 @@ namespace SeekerMAUI.Gamebook.NoYourGrace
                 return AvailabilityNode(option);
             }
         }
+
+        public List<string> VictoryPoints()
+        {
+            var results = new List<string> { "BOLD|CЧИТАЕМ ПОБЕДНЫЕ ОЧКИ:" };
+            var result = 0;
+
+            results.Add("BOLD|\nПриравниваем кол-во очков к Золоту:");
+            result = Character.Protagonist.Gold;
+            results.Add($"Золото = {Character.Protagonist.Gold}, соответетвенно Очки = {result}");
+
+            var prevResult = result;
+
+            results.Add("BOLD|Делим на десять:");
+            result /= 10;
+            results.Add($"{prevResult} / 10 = {result}");
+
+            prevResult = result;
+
+            results.Add("BOLD|Прибавляем к очкам значения параметра 'Даверн':");
+            result += Character.Protagonist.Davern;
+            results.Add($"{prevResult} + {Character.Protagonist.Davern} = {result}");
+
+            prevResult = result;
+
+            results.Add("BOLD|Добавляем к очкам кол-во недель:");
+            result += Character.Protagonist.Week;
+            results.Add($"{prevResult} + {Character.Protagonist.Week} = {result}");
+
+            prevResult = result;
+
+            results.Add("BOLD|Прибавляем к очкам кол-во подвигов:");
+            result += Character.Protagonist.Feats;
+            results.Add($"{prevResult} + {Character.Protagonist.Feats} = {result}");
+
+            var resultLine = Game.Services.CoinsNoun(result, "ое очко", "ых очка", "ых очков");
+            results.Add($"\nBIG|BOLD|ИТОГО:");
+            results.Add($"BIG|{result} победн{resultLine}!");
+
+            return results;
+        }
     }
 }
